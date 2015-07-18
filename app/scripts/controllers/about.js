51c2ab46db3f7305148215c39279943aa2455316
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-07-18 08:40:27 -0300

    Started the project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMongoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularMongoApp
+ */
+angular.module('angularMongoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMongoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMongoApp
+ */
+angular.module('angularMongoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
